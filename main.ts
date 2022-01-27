157fb583103771e2c765577c9b0544fd6b6775fb
ImAboy1286 <48336083+ImAboy1286@users.noreply.github.com>
2022-01-27 15:17:14 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+function BitUpdate () {
+    for (let index = 0; index <= 23; index++) {
+        if (BitArray[index]) {
+        	
+        } else {
+        	
+        }
+    }
+}
+let BitArray: number[] = []
+BitArray = []
+for (let index = 0; index < 25; index++) {
+    let list: boolean[] = []
+    list.push(false)
+}
+basic.showString("Bit Counting")
+basic.forever(function () {
+	
+})
